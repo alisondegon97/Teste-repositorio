00e2e141ea7f20e27f1c2546160a522e73046f94
alisondegon97 <alisondegon97@gmail.com>
2026-03-11 11:13:26 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+cont = 0
+nomes = []
+times = []
+
+for c in range(0, 5):
+    nome = str(input('Digite o nome de pessoa {}: \n'.format(c))).upper()
+    time = str(input('Digite o nome do time que {} escolheu: \n'.format(nome))).upper()
+    cont += 1 
+    nomes.append(nome)
+    times.append(time)
+
+for c in range (0, 5):
+    computadortime = random.choice(times)
+    computadornome = random.choice(nomes)
+    print('Computador escolheu o jogador {} joga com o time {}: '.format(computadornome, computadortime))
+    times.remove(computadortime)
+    nomes.remove(computadornome)
